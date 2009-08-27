042792080e095b4955bb4f23541bfb0a1c7ee3d6
Joel Larsson <bolmaster2@gmail.com>
2009-08-27 18:31:24 +0200

    added queuing functionality to lazyload

--- jquery.lazyloadimage.js
+++ jquery.lazyloadimage.js
@@ -3,23 +3,28 @@
  * @author joel
  * @param {Object} o
  */
-$.fn.lazyLoadImage = function(o) {
+$.fn.lazyLoadImage = function(o, callback) {
+	var self = this;
+	$.i = 0;
 	if (o == 'load') {
-		callback = typeof callback == 'undefined' ? $.callback : callback;
-		return this.each(function() {
-			$(this).attr('src', $(this).attr('original'));
-			//for ie if img is cached
-			if(this.complete) {
-				callback(this);
-			} else {
-				this.onload = function() {
-					callback(this);
-				};
-			}
-		});
+		callback = typeof callback == 'undefined' ? function(){} : callback;
+		
+		if ($.o.queue == true) {
+			$.queue($(this));
+			return;
+		} else {
+			return this.each(function() {
+				$.add(this);
+				$.callback(this, callback);
+			});
+		}
 	}
-	o = $.extend({nocache: false, queue: false, replaceImg: 'blank.gif'}, o);
+	o = $.o = $.extend({nocache: false, queue: false, replaceImg: 'blank.gif'}, o);
 	
+	$.add = function(el) {
+		$(el).attr('src', $(el).attr('original'));
+		$(el).removeAttr('original');
+	};
 	$.remove = function(el) {
 			$(el).attr('original', $(el).attr('src'));
 			if (o.replaceImg == null)
@@ -27,8 +32,27 @@ $.fn.lazyLoadImage = function(o) {
 			else 
 				$(el).attr('src', o.replaceImg);
 	};
-	$.callback = function(el) {
-		// callback
+	$.callback = function(el, callback) {
+		//for ie if img is cached
+		if(el.complete) {
+			callback(el);
+		} else {
+			el.onload = function() {
+				callback(this);
+			};
+		}
+	};
+	
+	$.queue = function(el) {
+		$.add(el[$.i]);
+		$.callback(el[$.i], function() {
+			$.i++;
+			//console.log('i: '+$.i+' self.length: '+self.length);
+			if ($.i >= self.length) {
+				return false;
+			}
+			$.queue(self);
+		});
 	};
 	return this.each(function() {
 		$.remove(this);
