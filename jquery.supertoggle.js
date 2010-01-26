a1b3eb42033345e146f083b3f7319d4056380a01
Joel Larsson <bolmaster2@gmail.com>
2010-01-26 15:24:13 +0100

    update the supertoggle to use the trigger elements href instead of id for the toggled elements id

--- jquery.supertoggle.js
+++ jquery.supertoggle.js
@@ -1,10 +1,22 @@
+/**
+ * SuperToggle - toggle element with a link - more info at http://wiki.github.com/blmstr/jquery-plugins/
+ * @author Joel Larsson
+ * 
+ */
 jQuery.superToggle = function() {
 	$(".superToggle").bind("click", function() {
-		var toggleEl = "#"+this.id+"-toggle";
-		if ($(toggleEl).css("display") != "none") {
-			 $(toggleEl).hide();
-		} else {
-			$(toggleEl).show();
+		
+		// look for the href to be used for the toggler element id
+		var hash = this.href.split('#');
+		var prefix = hash[hash.length-1];
+		
+		var toggleEl = $("#"+prefix);
+		if (toggleEl.css("display") != "none") {
+			toggleEl.addClass("inactive").removeClass("active").hide();
+			$(this).addClass("inactive").removeClass("active");
+		} else { 
+			toggleEl.addClass("active").removeClass("inactive").show();
+			$(this).addClass("active").removeClass("inactive");
 		}
 		return false;
 	});
